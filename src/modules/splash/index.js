da252a3b65e1337bf10a234eae2dd722dc9bf417
keyserfaty <keserfaty@gmail.com>
2018-11-10 11:48:38 -0500

    add redirect on splash

--- src/modules/splash/index.js
+++ src/modules/splash/index.js
@@ -4,29 +4,22 @@ import { View, Text } from 'react-native'
 import { styles } from './statics/styles'
 
 class SplashScreen extends Component {
-  // handleSplashLogic = async () => {
-  //   // const token = await AsyncStorage.getItem('token')
-    
-  //   // if (!token) {
-  //   //   return this.props.navigation.navigate('Auth', {
-  //   //     transition: 'default'
-  //   //   })
-  //   // } else {
-  //   //   return this.props.navigation.navigate('Questions', {
-  //   //     transition: 'default'
-  //   //   })
-  //   // }
-  // }
+  handleSplashLogic = async () => {
+    // Should handle token presence here if it was necessary
+    this.props.navigation.navigate('Questions', {
+      transition: 'default'
+    })
+  }
 
-  // componentDidMount () {
-  //   this.handleSplashLogic()
-  // }
+  componentDidMount () {
+    this.handleSplashLogic()
+  }
 
   render () {
     return (
       <View style={styles.container}>
         <View style={styles.titleContainer}>
-          <Text>Splash</Text>
+          <Text>Questions!</Text>
         </View>
       </View>
     )

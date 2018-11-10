087dc0cd9d5a5c598a653e315a734b77b1b0828f
keyserfaty <keserfaty@gmail.com>
2018-11-10 12:27:33 -0500

    add info from proprs

--- src/modules/questions/__tests__/components/Question.js
+++ src/modules/questions/__tests__/components/Question.js
@@ -4,6 +4,12 @@ import Questions from '../../components/Question/index'
 import renderer from 'react-test-renderer'
 
 test('Should return a List element', () => {
-  const tree = renderer.create(<Questions />).toJSON()
+  const props = {
+    category: 'Some category',
+    question: 'Some question', 
+    count: 1
+  }
+
+  const tree = renderer.create(<Questions {...props} />).toJSON()
   expect(tree).toMatchSnapshot()
 })
--- src/modules/questions/components/Question/index.js
+++ src/modules/questions/components/Question/index.js
@@ -6,15 +6,15 @@ import Button from '../../../../components/Button'
 import styles from './statics/styles'
 
 const Question = props => {
-  const { onNextStep } = props
+  const { onNextStep, category, question, count } = props
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Entertainment: Videogames</Text>
+      <Text style={styles.title}>{category}</Text>
       <View style={styles.question}>
-        <Text>Some question text</Text>
+        <Text>{question}</Text>
       </View>
-      <Text style={styles.count}>1 / 10</Text>
+      <Text style={styles.count}>{count} / 10</Text>
       <View style={styles.buttonsContainer}>
         <Button primary onPress={() => onNextStep(1)}>No</Button>
         <Button primary onPress={() => onNextStep(1)}>Yes</Button>

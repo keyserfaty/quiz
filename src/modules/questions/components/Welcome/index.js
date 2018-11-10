eb0eaedd1f50b9ab65d640dbb9eac99b2dcccb0a
keyserfaty <keserfaty@gmail.com>
2018-11-10 12:41:00 -0500

    update styles

--- src/modules/questions/components/Welcome/index.js
+++ src/modules/questions/components/Welcome/index.js
@@ -3,15 +3,23 @@ import { View, Text } from 'react-native'
 
 import Button from '../../../../components/Button'
 
+import styles from './statics/styles'
+
 const Welcome = props => {
   const { onNextStep } = props
 
   return (
-    <View>
-      <Text>Welcome to the Trivia Challenge!</Text>
-      <Text>You will be presented with 10 true and false questions.</Text>
-      <Text>Can you score 100%?</Text>
-      <Button primary onPress={() => onNextStep(1)}>Begin</Button>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text>Welcome to the Trivia Challenge!</Text>
+      </View>
+      <View style={styles.body}>
+        <Text>You will be presented with 10 true and false questions.</Text>
+        <Text>Can you score 100%?</Text>
+      </View>
+      <View style={styles.footer}>
+        <Button primary onPress={() => onNextStep(1)}>Begin</Button>
+      </View>
     </View>
   )
 }

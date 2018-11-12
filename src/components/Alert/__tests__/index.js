d585738382f18197c9d263db569b0a715f2a4f82
keyserfaty <keserfaty@gmail.com>
2018-11-11 20:22:02 -0800

    update some snapshots

--- src/components/Alert/__tests__/index.js
+++ src/components/Alert/__tests__/index.js
@@ -7,25 +7,7 @@ import Shallow from 'react-test-renderer/shallow'
 const renderer = new Shallow()
 
 test('Should render', () => {
-  let component = renderer.render(<Alert title='Some title' subtitle='Some subtitle' />)
-
-  expect(component).toMatchSnapshot('initial render')
-})
-
-test('Should only display a title', () => {
-  let component = renderer.render(<Alert title='Some title' />)
-
-  expect(component).toMatchSnapshot('initial render')
-})
-
-test('Should only display a subtitle', () => {
-  let component = renderer.render(<Alert subtitle='Some subtitle' />)
-
-  expect(component).toMatchSnapshot('initial render')
-})
-
-test('Should display a spinner', () => {
-  let component = renderer.render(<Alert title='Some title' spinner={true} />)
+  let component = renderer.render(<Alert msg='Some msg' />)
 
   expect(component).toMatchSnapshot('initial render')
 })

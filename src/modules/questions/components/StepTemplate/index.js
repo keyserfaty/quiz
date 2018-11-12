cdbd20e88980aaefb2f680881ae0cb6f1a118982
keyserfaty <keserfaty@gmail.com>
2018-11-11 19:29:51 -0800

    fix type error

--- src/modules/questions/components/StepTemplate/index.js
+++ src/modules/questions/components/StepTemplate/index.js
@@ -23,7 +23,7 @@ const StepTemplate = props => {
 }
 
 StepTemplate.propTypes = {
-  style: PropTypes.string,
+  style: PropTypes.object,
   header: PropTypes.any,
   footer: PropTypes.any,
   children: PropTypes.any
